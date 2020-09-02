ae5b93946357468f03e1c423d85dffde9b6c85d3
hongyue-qiu <994059641@qq.com>
2020-09-02 18:30:17 +0800

    [hongyue-qiu] fix= pix accessing property test.

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -13,6 +13,24 @@ public class GrammarExercise {
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         //在这编写实现代码
+        
+        String[] string1 = firstWordList.split(",");
+        String[] string2 = secondWordList.split(",");
+        List<String> firstWordLists = new ArrayList<String>();
+        List<String> secondWordLists = new ArrayList<String>();
+        Corllections.addAll(firstWordLists,string1);
+        Corllections.addAll(SecondWordLists,string2);
+
+        long count1 = firstWordLists.stream().forEach(str -> str.matches("[a-zA-Z]+")).count();
+        long count2 = secondWordLists.stream().forEach(str -> str.matches("[a-zA-Z]+")).count();
+        if(count1 < firstWordLists.size() || count2 < secondWordLists.size()) throw new error("input not valid");
+        
+        Corllections.sort(finalWordLists);
+        Corllections.sort(secondWordLists);
+        List<String> finalWordLists = new ArrayList<String>();
+        Stream<String> finalStream = Stream.concat(firstWordLists.stream(),secondWordLists.stream());
+        finalStream.distinct().forEach(str->System.out.print(str));
+
         return null;
     }
 }
